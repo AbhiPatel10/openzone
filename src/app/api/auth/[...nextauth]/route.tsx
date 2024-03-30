2d76e43fe90ff21f989f75653fc0450a3d841d99
Abhi Patel <mr.abhiofficial100@gmail.com>
2024-03-30 22:18:33 +0530

    Consoles added to check variables

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 
+console.log("GITHUB_CLIENT_ID", process.env.GITHUB_CLIENT_ID)
+console.log("GITHUB_CLIENT_SECRET", process.env.GITHUB_CLIENT_SECRET)
+
 const handler = NextAuth({
   providers: [
     GithubProvider({
