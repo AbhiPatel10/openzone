12aea59587a6dd52df5e3823309fa713b28bc15f
Komal Agarwal <komal.agarwal5115@gmail.com>
2024-05-16 01:37:41 +0530

    env set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeatureSection from "@/components/FeatureSection/page";
 import HeroSection from "@/components/HeroSection/page";
+import ContactSection from "@/components/ContactSection/page";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <FeatureSection />
+      <ContactSection />
     </main>
   );
 }
---
+++ src/components/ContactSection/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactSection = () => {
+  return (
+    <div>
+      <p>Hi</p>
+    </div>
+  )
+}
+
+export default ContactSection;
