00e33d714ff2f5c5292dbb3dcc3efbfa54ed8381
Komal Agarwal <komal.agarwal5115@gmail.com>
2024-05-18 01:14:35 +0530

    contact pg linked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import FeatureSection from "@/components/FeatureSection/page";
 import HeroSection from "@/components/HeroSection/page";
-import ContactSection from "@/components/ContactSection/page";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <FeatureSection />
-      <ContactSection />
     </main>
   );
 }
