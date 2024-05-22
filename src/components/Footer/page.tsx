0fba76ca23cb7af6304404c647366d5bbd4cf16b
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-05-23 00:42:49 +0530

    Fix/reomved social links and updated mail.

--- src/components/Footer/page.tsx
+++ src/components/Footer/page.tsx
@@ -87,19 +87,19 @@ const Footer = () => {
           <Link href="https://github.com/AbhiPatel10/openzone" target='_blank'>
             <Github className="h-10 w-12 text-slate-300 rounded-full border-2 p-[5px] hover:transition duration-200 ease hover:bg-slate-600" />
           </Link>
-          <Link href="mailto:mr.abhiofficial100@gmail.com" target=''>
+          <Link href="mailto:abhipatel.dev@gmail.com" target=''>
             <Mail className="h-10 w-12 text-slate-300 rounded-full border-2 p-[5px] hover:transition duration-200 ease hover:bg-slate-600" />
           </Link>
           <Link href="https://www.facebook.com" target='_blank'>
             <Facebook className="h-10 w-12 text-slate-300 rounded-full border-2 p-[5px] hover:transition duration-200 ease hover:bg-slate-600" />
           </Link>
-          <Link href="https://twitter.com/AbhiPatel0001" target='_blank'>
+          <Link href="https://twitter.com" target='_blank'>
             <Twitter className="h-10 w-12 text-slate-300 rounded-full border-2 p-[5px] hover:transition duration-200 ease hover:bg-slate-600" />
           </Link>
-          <Link href="https://www.instagram.com/_.abhipatell._/" target='_blank'>
+          <Link href="https://www.instagram.com" target='_blank'>
             <Instagram className="h-10 w-12 text-slate-300 rounded-full border-2 p-[5px] hover:transition duration-200 ease hover:bg-slate-600" />
           </Link>
-          <Link href="https://www.linkedin.com/in/abhipatel001/" target='_blank'>
+          <Link href="https://www.linkedin.com/in" target='_blank'>
             <Linkedin className="h-10 w-12 text-slate-300 rounded-full border-2 p-[5px] hover:transition duration-200 ease hover:bg-slate-600" />
           </Link>
         </div>
