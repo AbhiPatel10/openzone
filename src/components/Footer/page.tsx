54f3be6a1d9437bc4c752468e7ca52574f4c7525
Piyush-Linux <piyushpatil98@protonmail.com>
2024-05-28 13:27:17 +0530

    feat: Footer UI Improve

--- src/components/Footer/page.tsx
+++ src/components/Footer/page.tsx
@@ -10,15 +10,16 @@ const Footer = () => {
       className="
       container
       flex flex-col flex-wrap
+      justify-evenly
       px-8
-      py-8
+      py-14
       mx-auto
       md:items-center
       lg:items-start
       md:flex-row md:flex-nowrap
     "
     >
-      <div className="flex-shrink-0 w-96 mx-auto text-center md:mx-0 md:text-left">
+      <div className="flex-shrink-0 w-96 mx-auto md:mx-0 md:text-left">
         <Link href={"/"} className="text-3xl text-white">
             OpenZone
         </Link>
@@ -26,34 +27,34 @@ const Footer = () => {
           Discover, Contribute, Thrive - OpenZone: Your gateway to open-source collaboration. Empowering both seasoned contributors and beginners alike to explore, engage, and make a difference in the world of open-source projects.
         </p>
       </div>
-      <div className="flex justify-evenly w-full mt-8 text-center lg:flex flex-wrap max-md:mt-16">
+      <div className="flex justify-evenly w-full lg:flex flex-wrap max-md:mt-16">
         <div className="w-full px-4 lg:w-1/3 md:w-1/2">
           <h2 className="mb-2 font-bold tracking-widest text-lg text-gray-100">
             Quick Links
           </h2>
           <ul className="mb-8 space-y-2 text-s list-none">
             <li>
-              <Link href={"/"} className="text-gray-400">
+              <Link href={"/"} className="text-gray-400 hover:text-gray-500">
                 Home
               </Link>
             </li>
             <li>
-              <Link href={"/projects"} className="text-gray-400" target='_blank'>
+              <Link href={"/projects"} className="text-gray-400 hover:text-gray-500" target='_blank'>
                 Projects
               </Link>
             </li>
             <li>
-              <Link href={"/ContactSection"} className="text-gray-400" target='_blank'>
+              <Link href={"/ContactSection"} className="text-gray-400 hover:text-gray-500" target='_blank'>
                 Contact Us
               </Link>
             </li>
             <li>
-              <Link href={"PrivacyPolicy"} className="text-gray-400" >
+              <Link href={"PrivacyPolicy"} className="text-gray-400 hover:text-gray-500" >
                 Privacy Policy
               </Link>
             </li>
             <li>
-              <Link href={"TermsAndConditions"} className="text-gray-400" >
+              <Link href={"TermsAndConditions"} className="text-gray-400 hover:text-gray-500" >
                 Terms and Conditions
               </Link>
             </li>
@@ -65,17 +66,17 @@ const Footer = () => {
           </h2>
           <ul className="mb-8 space-y-2 text-s list-none">
             <li>
-              <Link href="https://github.com/AbhiPatel10/openzone/blob/main/README.md" className="text-gray-400" target='_blank'>
+              <Link href="https://github.com/AbhiPatel10/openzone/blob/main/README.md" className="text-gray-400 hover:text-gray-500" target='_blank'>
                 Documentation
               </Link>
             </li>
             <li>
-              <Link href="https://github.com/AbhiPatel10/openzone/issues" className="text-gray-400" target='_blank'>
+              <Link href="https://github.com/AbhiPatel10/openzone/issues" className="text-gray-400 hover:text-gray-500" target='_blank'>
                 GitHub Issues
               </Link>
             </li>
             <li>
-              <Link href="https://github.com/AbhiPatel10/openzone/pulls" className="text-gray-400" target='_blank'>
+              <Link href="https://github.com/AbhiPatel10/openzone/pulls" className="text-gray-400 hover:text-gray-500" target='_blank'>
                 GitHub PRs
               </Link>
             </li>
