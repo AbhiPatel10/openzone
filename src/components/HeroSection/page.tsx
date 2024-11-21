ac53d929e0f9d3a3675fac4a801c4b865e88184b
Hani-3 <hanypatel1603@gmail.com>
2024-11-21 23:48:54 +0530

    Fix: basic css changes

--- src/components/HeroSection/page.tsx
+++ src/components/HeroSection/page.tsx
@@ -2,12 +2,12 @@ import { Button } from "@/components/ui/button";
 
 export default function HeroSection() {
   return (
-    <section className="bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 ... h-screen flex flex-col justify-center items-center text-center p-4 w-full">
+    <section className="bg-gradient-to-bl from-[#070707] via-[#020c2d] to-[#000001] ... h-[92vh] flex flex-col justify-center items-center text-center p-4 w-full">
       <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-white">
-        Empower Open Source. Build Together.
+        Empower <span className="text-secondary">Open Source</span>. Build Together.
       </h1>
       <p className="text-lg md:text-xl lg:text-2xl text-white mt-4 mr-2 md:mt-6 lg:mt-8 underline decoration-secondary underline-offset-8">
-        Open Zone: Connect, Collaborate, Contribute.
+        Connect - Collaborate - Contribute.
       </p>
       <Button
         className="mt-8 text-black bg-secondary border-primary hover:scale-125 hover:bg-secondary transition-transform ease-in-out rounded-full"
